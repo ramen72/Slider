4aa8b066676f4efca7001d5006a362224f3973a9
ramen72 <ramen@devsramen.com>
2025-03-11 15:59:56 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let slides = document.querySelectorAll('.slides');
+    let dotsContainer = document.querySelector('.dots');
+    let prevButton = document.querySelector('.prev');
+    let nextButton = document.querySelector('.next');
+    let slide = document.querySelectorAll('.slide');
+    let slideCount = slide.length 
+    console.log(slide.length)
+})
